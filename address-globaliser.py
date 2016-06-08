c1a627996dea42f46bb71f8088b8e729c8497fb5
Ben Dale <ben.dale@gmail.com>
2016-06-08 17:27:59 +1000

    Updated to work with address-sets that include address-sets

--- address-globaliser.py
+++ address-globaliser.py
@@ -29,50 +29,54 @@ if inetRegex.match(str(sys.argv[1])):
     dev.open()		
     config = dev.rpc.get_config(filter_xml=etree.XML('<configuration><security><zones/></security></configuration>'))
     dev.close()
-    addresses = config.xpath('//address-book/address')
-    for address in addresses:
-        address_name = address.find('name').text
-        if address_name != None:
-            if (address.find('ip-prefix') != None):
-                # This is an ip-prefix address
-                address_prefix = address.find('ip-prefix').text
-                address_entries.append({'name':address_name, 'ip-prefix':address_prefix})
-                #print ("Address Name: {}, Address Prefix: {}".format(address_name, address_prefix))
-                sys.stdout.write(".")
-            elif (address.find('dns-name/name') != None):
-                # This is a dns-name address
-                dns_name = address.find('dns-name/name').text
-                address_entries.append({'name':address_name, 'dns-name':dns_name})            
-                sys.stdout.write(".")
-                #print ("Address Name: {}, DNS Name: {}".format(address_name, dns_name))
-            elif (address.find('wildcard-address/name') != None):
-                # This is a wildcard address
-                wildcard_address = address.find('wildcard-address/name').text
-                address_entries.append({'name':address_name, 'wildcard-address':wildcard_address})            
-                sys.stdout.write(".")
-                #print ("Address Name: {}, Wildcard Address: {}".format(address_name, wildcard_address))
-            elif (address.find('range-address/name') != None):
-                # This is a range address
-                range_address = address.find('range-address/name').text
-                high_address = address.find('range-address/to/range-high').text
-                address_entries.append({'name':address_name, 'range-address':range_address, 'range-high':high_address})            
-                sys.stdout.write(".")
-                #print ("Address Name: {}, Range Address: {} to {}".format(address_name, range_address, high_address))                
-            else:
-                print "Address-type unknown"
-    #print address_entries
-    address_sets = config.xpath('address-book/address/address-set')
-    print "\n"
-else:	
+    zones = config.xpath('//security-zone')
+    for zone in zones:
+        zone_name = zone.find('name').text
+        addresses = zone.xpath('address-book/address')
+        if addresses:
+            for address in addresses:
+                address_name = address.find('name').text
+                if address_name != None:
+                    if (address.find('ip-prefix') != None):
+                        # This is an ip-prefix address
+                        address_prefix = address.find('ip-prefix').text
+                        print ("set security address-book global address {} {}".format(address_name, address_prefix))
+                    elif (address.find('dns-name/name') != None):
+                        # This is a dns-name address
+                        dns_name = address.find('dns-name/name').text
+                        print ("set security address-book global address {} dns-name {}".format(address_name, dns_name))
+                    elif (address.find('wildcard-address/name') != None):
+                        # This is a wildcard address
+                        wildcard_address = address.find('wildcard-address/name').text        
+                        print ("set security address-book global address {} wildcard-address {}".format(address_name, wildcard_address))
+                    elif (address.find('range-address/name') != None):
+                        # This is a range address
+                        range_address = address.find('range-address/name').text
+                        high_address = address.find('range-address/to/range-high').text
+                        print ("set security address-book global address {} range-address {} to {}".format(address_name, range_address, high_address))
+                    else:
+                        print "Address-type unknown"
+                else:
+                    pass
+                    # no addresses found
+            address_sets = zone.xpath('address-book/address-set')
+            for address_set in address_sets:
+                address_set_name = address_set.find('name').text
+                member_addresses = address_set.xpath('address')
+                for member_address in member_addresses:
+                    member_address_name = member_address.find('name').text
+                    print ("set security address-book global address-set {} address {}".format(address_set_name, member_address_name)) 
+                member_address_sets = address_set.xpath('address-set')
+                for member_address_set in member_address_sets:
+                    member_address_set_name = member_address_set.find('name').text
+                    print ("set security address-book global address-set {} address-set {}".format(address_set_name, member_address_set_name))
+        # delete the zone-based address-book
+            print ("delete security zones security-zone {} address-book".format(zone_name))
+        else:
+            # no addresses in this zone
+            pass
+    # done 
+    print ("\n")
+else:
+    # ip-regex failed	
 	print "Invalid IP Address"
-
-#Pastey
-#from jnpr.junos import Device
-#from lxml import etree
-#dev=Device(host='192.168.56.100',user='lattice',passwd='lattice123')
-#dev.open()		
-#config = dev.rpc.get_config(filter_xml=etree.XML('<configuration><security><zones/></security></configuration>'))
-    
-    
-#for element in config.iter("{*}address-book/address"):
-#    print("Address Name: {}, Address: {}".format(element.find('name').text, element.find('{*}ip-prefix').text))
